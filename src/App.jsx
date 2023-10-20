851c5c5663692b92488823eab6f8638d027252bb
Scelo <scelogumede95@gmail.com>
2023-10-20 22:57:07 +0200

    just now

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home';
 import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
+import Header from './components/Header';
 
 function App() {
   return (
   <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/sign-in' element={<Signin />}/>
